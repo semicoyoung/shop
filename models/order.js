dd1cd14c0d25ed7300b8079c1ea2abe912796f52
young <eetyang@163.com>
2015-06-03 14:30:27 +0800

    修改购物车，修改订单(6.3下午)

--- models/order.js
+++ models/order.js
@@ -7,6 +7,8 @@ function Order(uname, order) {
 
 module.exports = Order;
 
+
+
 Order.save = function (carts, ip, who, phone, callback) {
 
     var date = new Date();
@@ -24,11 +26,11 @@ Order.save = function (carts, ip, who, phone, callback) {
         if (err) {
             callback(err);
         }
-        db.collection('orders', function (err, collection) {
+        db.collection('orders', function(err, collection) {
             if (err) {
                 callback(err);
             }
-            carts.forEach(function (cart, index) {
+            carts.forEach(function(cart, index) {
                 var order = {
                     uname: cart.uname,
                     cid: cart.cid,
@@ -52,6 +54,67 @@ Order.save = function (carts, ip, who, phone, callback) {
         });
     });
 };
+
+
+/*
+Order.save=function(carts,ip,who,phone,callback){
+    console.log(carts[0].cid);
+    mongodb.open(function(err,db){
+
+        if(err){
+            callback(err);
+        }
+
+        db.collection('ids',function(err,collection){
+            if(err){
+                mongodb.close();
+                console.log("wrong");
+                callback(err,null);
+            }
+
+            collection.findOne({"name":"order"},function(err,doc){
+                var  id=parseInt(doc.id);
+                console.log(id);
+                collection.update({"name":"order"},{$inc:{'id':carts.length}});
+
+                db.collection('orders',function(err,collection){
+                    var date=new Date();
+
+                    var time={
+                        date:date,
+                        year:date.getFullYear(),
+                        month:date.getFullYear()+"-"+(date.getMonth()+1),
+                        day : date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate(),
+                        minute : date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes()
+                    };
+
+                    carts.forEach(function(cart,index){
+                        id=id+1;
+                        var order={
+                            "oid":id,
+                            "uname":cart.uname,
+                            "cid":cart.cid,
+                            "cname":cart.cname,
+                            "cimage":cart.cimage,
+                            "cprice":cart.cprice,
+                            "camount":cart.camount,
+                            "ip":ip,
+                            "who":who,
+                            "phone":phone,
+                            "time":time
+                        };
+                        collection.insert(order);
+                    });
+
+                    mongodb.close();
+                    callback(null,1);
+                });
+            });
+        });
+    });
+};
+
+*/
 
 Order.get = function (uname, callback) {
     mongodb.open(function (err, db) {
